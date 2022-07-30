53014f5c418c6bf2aa60b75f57518b556b108ebc
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-31 01:31:11 +0530

    File updated

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -33,3 +33,14 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   port             = 80
 }
 
+
+resource "aws_lb_listener" "lb-listner" {
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
