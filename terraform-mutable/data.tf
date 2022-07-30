5db0dcf39697dd4d50d49205e75fe39d3e05ca89
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-31 00:07:38 +0530

    File updated

--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -4,3 +4,12 @@ data "aws_ami" "ami" {
   owners           = ["self"]
 
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-kartikayj"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -1,9 +1,19 @@
 resource "aws_spot_instance_request" "ec2-spot" {
-  count = var.INSTANCE_COUNT
+  count         = var.INSTANCE_COUNT
   ami           = data.aws_ami.ami.id
   instance_type = var.INSTANCE_TYPE
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}-${count.index+1}"
   }
+  subnet_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS[count.index]
+}
+
+
+
+resource "aws_ec2_tag" "spot-instances" {
+  count       = length(aws_spot_instance_request.ec2-spot)
+  resource_id = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
+  key         = "Name"
+  value       = "${var.COMPONENT}-${var.ENV}-${count.index+1}"
 }
