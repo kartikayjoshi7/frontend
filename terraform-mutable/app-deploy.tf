44254de218c1f9a1696801209ab7ea499d0f59ff
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-31 02:34:36 +0530

    File updated

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -8,9 +8,8 @@ resource "null_resource" "app-deploy" {
       host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
     }
     inline = [
-      "ansible-pull -U https://github.com/kartikayjoshi7/ansible.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}",
-      "consul join ${aws_instance.web.private_ip}",
+      "ansible-pull -U https://github.com/kartikayjoshi7/ansible.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
+
     ]
   }
-}
 }
